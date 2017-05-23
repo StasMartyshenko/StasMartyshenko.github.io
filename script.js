2e40e21f358c94590138ff5b4e722666ad73a841
StasMartyshenko <martyshenkostas@gmail.com>
2017-05-23 15:05:34 +0300

    commit

---
+++ script.js
@@ -0,0 +1,87 @@
+//---------navigation-------------
+
+$(function () {
+
+    $('.menuToggle').on('click', function () {
+
+        $('.menu').slideToggle(300, function () {
+
+            if ($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+
+        });
+
+    });
+
+});
+
+
+//-----------Slides--------------
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+//--------------------sl-slider------------------
+$('.sl').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 8,
+  slidesToScroll: 8,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
